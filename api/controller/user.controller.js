b642bb0ea4186e8f582f4b985676b212ac958a31
splenwilz <godswill.williamsz@gmail.com>
2023-06-09 11:24:41 +0000

    Hashed Password

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,6 +1,11 @@
 const httpStatusCodes = require('http-status-code');
 const user = require('../models/user.model');
 
+
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 // This will be auto generated and added to users account on creation
 
 const API_KEY = "67945731797be1APIKEy";
@@ -23,8 +28,15 @@ class UserController{
     }
 
     register = (req, res) =>{
-        const body = req.body;
-        user.create(body).then(docs => {
+
+       
+
+
+        const body = req.body;        
+        console.log(body);
+        const hash = bcrypt.hashSync(body.password, saltRounds);
+        const body2 = { email: body.email, password: hash };
+        user.create(body2).then(docs => {
             return res.status(201).send(docs);
         }).catch(err => {
             return res.status(500).send({
@@ -35,6 +47,14 @@ class UserController{
       
 
     create = (req, res)=>{
+
+        const apiKey = req.header("x-api-key");
+
+        if (!apiKey || apiKey !== API_KEY) {
+            return res.status(401).json({ message: "Unauthorized Access, Route Requires API Key" });
+        }
+
+
         const body = req.body;
         user.create(body).then(docs => {
             return res.status(201).send(docs);
