d0a351d6c2a980a9f70bc2b4a41f84cda10ba785
Godswill William <godswill.williamsz@gmail.com>
2023-06-16 09:53:50 +0000

    served static site for deployment app.js

--- api/app.js
+++ api/app.js
@@ -1,8 +1,37 @@
-const express = require('express');
-let cors = require('cors');
-require('dotenv').config();
+// const express = require('express');
+// let cors = require('cors');
+// require('dotenv').config();
 
-require('./config/db')
+// require('./config/db')
+
+// const apiRoutes = require('./routes/api.routes');
+// const app = express();
+// app.use(express.json());
+// app.use(cors());
+// app.use('/api', apiRoutes);
+
+// const port = process.env.PORT || 3000;
+
+// app.use(express.static(path.join(__dirname, 'build')));
+
+// -app.get('/', function (req, res) {
+// +app.get('/*', function (req, res) {
+//    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+//  });
+
+
+
+// app.listen(port, () => {
+//     console.log(`Server is running at localhost:${port}`);
+// })
+
+
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const path = require('path');
+
+require('./config/db');
 
 const apiRoutes = require('./routes/api.routes');
 const app = express();
@@ -12,6 +41,12 @@ app.use('/api', apiRoutes);
 
 const port = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.listen(port, () => {
-    console.log(`Server is running at localhost:${port}`);
-})
+  console.log(`Server is running at localhost:${port}`);
+});
